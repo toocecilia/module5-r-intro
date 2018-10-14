e89989dc6da135e967f6978997c09e8d45654b9f
Cecilia Too <toocecilia@gmail.com>
2018-10-13 21:13:51 -0700

    Completed Exercises 1 and 2

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,24 +1,25 @@
 # Exercise-1: practice with basic syntax
 
 # Create a variable `hometown` that stores the city in which you were born
-
+hometown <- "Georgetown"
 
 # Assign your name to the variable `my.name`
-
+my.name <- "Cecilia Too"
 
 # Assign your height (in inches) to a variable `my.height`
-
+my.height <- 50
 
 # Create a variable `puppies` equal to the number of puppies you'd like to have
-
+puppies <- 3
 
 # Create a variable `puppy.price`, which is how expensive you think a puppy is
-
+puppy.price <- 100.50
 
 # Create a variable `total.cost` that has the total cost of all of your puppies
-
+total.cost <- puppies * puppy.price
 
 # Create a boolean variable `too.expensive`, set to TRUE if the cost is greater than $1,000
-
+too.expensive <- FALSE
 
 # Create a variable `max.puppies`, which is the number of puppies you can afford for $1,000.
+max.puppies <- 10
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,28 +1,29 @@
 # Exercise-2: more practice with basic syntax
 
 # Create a variable `food` that stores your favorite kind of food
-
+food <- "Chicken curry and rice"
 
 # Create a variable 'restaurant' that stores your favorite place to eat
-
+restaurant <- 'Monga Cafe'
 
 # Create a variable `friends` equal to the number of friends you would like to eat with
-
+friends <- "five"
 
 # Create a variable `meal.price`, which is how expensive you think one meal at the restaurant will be
-
+meal.price <- 10.75
 
 # Create a variable `total.cost` that has the total cost of your bill
-
+total.cost <- meal.price
 
 # Create a variable 'total.cost.tip' to be the total cost including a 15% tip
-
+total.cost.tip <- total.cost + (total.cost * 0.15)
 
 # Create a variable 'price.limit' set to your spending budget
-
+price.limit <- 150
 
 # Create a boolean variable `too.expensive`, set to TRUE if the cost with the tip is greater than the price limit
-
+too.expensive = FALSE
 
 # Create a variable `max.friends`, which is the maximum number of friends you can invite that is in range
 # of your price limit
+max.friends <- price.limit / total.cost.tip
